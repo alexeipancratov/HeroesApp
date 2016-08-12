de91dc48a652e37b96e25eea23704fd78d321309
Alexei Pancratov <drivergear@gmail.com>
2016-08-12 17:07:37 +0300

    Implemented method to return single hero

--- app/heroes/hero.service.ts
+++ app/heroes/hero.service.ts
@@ -8,12 +8,21 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class HeroService {
     private _heroesUrl = 'api/heroes/heroes.json';
+    private _heroUrl = 'api/heroes/hero';
 
     constructor(private _http: Http) { }
 
     getHeroes(): Observable<IHero[]> {
         return this._http.get(this._heroesUrl)
             .map(response => <IHero[]>response.json())
+            .catch(this.handleError);
+    }
+
+    getHero(id: number): Observable<IHero> {
+        let url = `${this._heroUrl}${id}.json`;
+
+        return this._http.get(url)
+            .map(response => <IHero>response.json())
             .catch(this.handleError);
     }
 
